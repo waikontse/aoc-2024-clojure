956d62d8cce4c66d991f14e041b712641df87163
wkt <waikon@waikontse.info>
2024-12-04 23:38:53 +0100

    Add solution for day 3 part 1.

--- src/advent_of_code_2024/week1/day3.clj
+++ src/advent_of_code_2024/week1/day3.clj
@@ -1,22 +1,40 @@
-(ns advent-of-code-2024.week1.day3)
+(ns advent-of-code-2024.week1.day3
+  (:require [advent-of-code-2024.utils.io :as io]))
+
+(defn parse-op
+  [raw-op]
+  (let [raw-operands (-> raw-op
+                         (clojure.string/replace #"mul\(" "")
+                         (clojure.string/replace #"\)" "")
+                         (clojure.string/split #","))
+        ]
+    {:op "mul" :left (first raw-operands) :right (second raw-operands)}))
 
 (defn parse-line
   [raw-line]
   "return a map of operands eg: {:left <int> :right <int> :op mul}"
-  )
+  (let [operands (re-seq #"mul\(\d{1,3},\d{1,3}\)" raw-line)]
+    (map parse-op operands)))
 
 (defn execute-operand
-  "docstring"
-  [arglist]
-  )
+  [operand]
+  ;(println "executing op: " operand)
+  (* (io/str->int (:left operand)) (io/str->int (:right operand))))
 
 (defn solve-part-1
   "docstring"
   [filename]
-  0)
-
+  (let [raw-lines (io/read-input filename)
+        parsed-operands (flatten (map parse-line raw-lines))
+        results (map execute-operand parsed-operands)
+        ]
+    ;(run! println parsed-operands)
+    ;(println results)
+    (reduce + results)))
 
 (defn solve-part-2
   "docstring"
   [filename]
   0)
+
+(parse-line "mul(123,123)")
--- test/advent_of_code_2024/week1/day3_test.clj
+++ test/advent_of_code_2024/week1/day3_test.clj
@@ -2,17 +2,17 @@
   (:require [clojure.test :refer :all])
   (:require [advent-of-code-2024.week1.day3 :refer :all]))
 
-(deftest solve-part-1-test
+(deftest solve-part-1-example-test
   (testing "solution for day 3 part 1")
   (is (= (solve-part-1 "day3/example.txt") 161)))
 
-(deftest solve-part-1-test
+(deftest solve-part-1-input-test
   (testing "solution for day 3 part 1")
-  (is (= (solve-part-1 "day3/input.txt") 161)))
+  (is (= (solve-part-1 "day3/input.txt") 173785482)))
 
-(deftest solve-part-2-test
+(deftest solve-part-2-example-test
   (testing "solution for day 3 part 2")
-  (is (= (solve-part-2 "day3/example.txt") 161)))
+  (is (= (solve-part-2 "day3/example.txt") 48)))
 
 
 (deftest solve-part-2-test
