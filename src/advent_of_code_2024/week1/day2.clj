27359ffbdc19bdcf7b3a6ce0c0dc71703d3b9a40
wkt <waikon@waikontse.info>
2024-12-04 08:08:40 +0100

    refactored code for day 2 part 1 to easier solve part 2.

--- src/advent_of_code_2024/week1/day2.clj
+++ src/advent_of_code_2024/week1/day2.clj
@@ -3,47 +3,49 @@
             [clojure.string :as str]
             [clojure.core.reducers :as r]))
 
-(defn replace-val
-  [truths]
-  (let [position (.indexOf truths false)
-        _ (println truths)]
-    (if (> position -1)
-      (assoc truths position true)
-      truths)
-    )
+(defn parse-line
+  [raw-line]
+  (->> (str/split raw-line #" ")
+       (map io/str->int))
   )
-
-(defn is-strictly?
-  [f my-comp max numbers compensator]
-  (let [less-then-max (map #(my-comp % max) numbers)
-        are-strictly-numbers (map f numbers)
-        combined (into [] (r/flatten (map vector less-then-max are-strictly-numbers)))
-        _ (println numbers)
-        _ (println "combined: " combined)
-        result (every? identity (compensator combined))
-        ]
-    ()
-    result))
 
 (defn calc-diff
   [arglist]
   (- (first arglist) (second arglist)))
 
+(defn is-strictly?
+  [f my-comp max numbers]
+  (let [less-then-max (map #(my-comp % max) numbers)
+        are-strictly-numbers (map f numbers)
+        ; _ (println are-strictly-numbers)
+        combined (into [] (r/flatten (map vector less-then-max are-strictly-numbers)))
+        ;     _ (println numbers)
+        ;    _ (println "combined: " combined)
+        result (every? identity combined)
+        ]
+    ()
+    result))
+
+(defn is-safe?
+  [numbers]
+  (let [diff-nums (->> numbers
+                       (partition 2 1)
+                       (map calc-diff))
+        is-positively-safe (is-strictly? pos-int? <= 3 diff-nums)
+        is-negatively-safe (is-strictly? neg-int? >= -3 diff-nums)
+        ]
+    (or is-positively-safe is-negatively-safe))
+  )
+
 (defn solve-part-1
   [filename]
   (println "solving part 1 with filename: " filename)
-  (let [data (io/read-input filename)
-        clean-data (->>
-                     data
-                     (map #(str/split % #" "))
-                     (map #(map io/str->int %))
-                     (map #(partition 2 1 %)))
-        calc-data (map #(map calc-diff %) clean-data)
-        is-strictly-positive (count (filter #(is-strictly? pos-int? <= 3 % identity) calc-data))
-        is-strictly-negative (count (filter #(is-strictly? neg-int? >= -3 % identity) calc-data))
+  (let [raw-lines (io/read-input filename)
+        parsed-lines (map parse-line raw-lines)
+        all-safety-values (map is-safe? parsed-lines)
         ]
-    (+ is-strictly-positive is-strictly-negative))
-  )
+    (count (filter true? all-safety-values))
+    ))
 
 (defn solve-part-2
   [filename]
@@ -54,17 +56,17 @@
                      (map #(map io/str->int %))
                      (map #(partition 2 1 %)))
         calc-data (map #(map calc-diff %) clean-data)
-        is-strictly-positive (count (filter #(is-strictly? pos-int? <= 3 % replace-val) calc-data))
+        is-strictly-positive (count (filter #(is-strictly? pos-int? <= 3 %) calc-data))
         _ (println calc-data)
         _ (println "positive: " is-strictly-positive)
-        is-strictly-negative (count (filter #(is-strictly? neg-int? >= -3 % replace-val) calc-data))
+        is-strictly-negative (count (filter #(is-strictly? neg-int? >= -3 %) calc-data))
         ]
     (+ is-strictly-negative is-strictly-positive)))
 
 
 ;;(def sample (slurp "resources/day2/example.txt"))
 
-;; Solution from Thomas van der Veen
+;; Solution from Thomas
 
 ;(def all (slurp "input.txt"))
 ;(def small (slurp "small.txt"))
