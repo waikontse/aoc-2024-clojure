8e86c9cc803221f0a0630bf11826275429e1f973
wkt <waikon@waikontse.info>
2025-01-01 23:12:32 +0100

    Add partial solution for day 8

--- src/advent_of_code_2024/week2/day8.clj
+++ src/advent_of_code_2024/week2/day8.clj
@@ -1,29 +1,55 @@
-(ns advent-of-code-2024.week2.day8)
-
-(defn parse-board
-  "docstring"
-  [arglist]
-  )
+(ns advent-of-code-2024.week2.day8
+  (:require [advent-of-code-2024.utils.board :as board]
+            [advent-of-code-2024.utils.io :as io]))
 
 (defn find-all-distinct-symbols
   "docstring"
-  [arglist]
-  )
+  [board]
+  (let [all-symbols (->>
+                      (for [x-pos (range (:width board))
+                            y-pos (range (:height board))]
+                        (let [symbol (board/get-pos x-pos y-pos board)]
+                          (when (not= \. symbol)
+                            {:symbol symbol :x-pos x-pos :y-pos y-pos})))
+                      (filter some?))]
+    (group-by :symbol all-symbols)))
 
-(defn find-all-same-symbols
+(defn generate-anti-nodes
   "docstring"
-  [arglist]
-  )
+  [left right]
+  (println "generating anti-nodes" left right)
+  (let [x-diff (- (:x-pos left) (:x-pos right))
+        y-diff (- (:y-pos left (:y-pos right)))
+        new-left-position {:x-pos (+ (:x-pos left) x-diff) :y-pos (+ (:y-pos left) y-diff)}
+        new-right-position {:x-pos (- (:x-pos right) x-diff) :y-pos (- (:y-pos right) y-diff)}
+        ]
+    [new-left-position new-right-position]))
 
-(defn generate-all-antinodes
+(defn generate-all-anti-nodes
   "docstring"
-  [arglist]
+  [positions]
+  (println "generating all-anti-nodes" positions)
+  (loop [elem1 (first positions)
+         xs (rest positions)
+         acc []]
+    (let [combinations (when (not-empty xs) (map (fn [elem] [elem1 elem]) xs))
+          _ (println "combinations:" combinations)]
+      (if (empty? xs)
+        acc
+        (recur (first xs)
+               (rest xs)
+               (into acc (map #(generate-anti-nodes (first %) (second %)) combinations)))))
+    )
   )
 
 (defn solve-part-1
   "docstring"
   [filename]
-  0)
+  (let [raw-lines (io/read-input "day8/example.txt")
+        board (board/parse-to-board raw-lines)
+        all-distinct-symbols (find-all-distinct-symbols board)]
+    (generate-all-anti-nodes (get all-distinct-symbols \0)))
+  )
 
 
 (defn solve-part-2
