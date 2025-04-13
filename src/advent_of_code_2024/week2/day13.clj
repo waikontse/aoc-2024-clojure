112b7bb165b2d5e74b2d24ee78162b0f325eb482
Wai Kon Tse <wai.kon.tse@ing.com>
2025-04-13 08:07:18 +0200

    Added more functionality for day 13

--- src/advent_of_code_2024/week2/day13.clj
+++ src/advent_of_code_2024/week2/day13.clj
@@ -28,65 +28,67 @@
 
 (defn ints->expanded-form
   [xs]
-  (let [first-row (map #(first %) xs)
-        second-row (map #(second %) xs)
-        ]
-    [first-row second-row]))
+  (apply map vector xs))
 
-(defn matrix-divide-by
-  [matrix divisor]
-  (map #(/ % divisor) matrix))
+(ints->expanded-form '((94 34) (22 67) (8400 5400)))
+(ints->expanded-form [[94 34] [22 67] [8400 5400]])
 
-(defn matrix-multiply-by
-  [matrix multiplier]
-  (map #(* % multiplier) matrix))
+(defn row-divide-by
+  [row divisor]
+  (map #(/ % divisor) row))
 
-(defn matrix-add
+(defn row-multiply-by
+  [row multiplier]
+  (map #(* % multiplier) row))
+
+(defn row-add
   [m1 m2]
   (->> (map vector m1 m2)
        (map #(apply + %))))
 
-(defn matrix-subtract
+(defn row-subtract
   [m1 m2]
   (->> (map vector m1 m2)
        (map #(apply - %))))
 
+(defn row-simplify-col
+  [row col]
+  (let [divisor (nth row col)]
+    (row-divide-by row divisor)))
 
-(defn matrix-simplify-col
+(defn matrix-reduce-col
+  "docstring"
   [matrix col]
-  (let [divisor (nth matrix col)
-        ]
-    (matrix-divide-by matrix divisor)))
+  (let [target (row-simplify-col matrix col)]
+    )
+  )
 
-(matrix-simplify-col [94 22 8400] 0)
-(matrix-divide-by [94 22 8400] 94)
+(row-simplify-col [94 22 8400] 0)
+(row-divide-by [94 22 8400] 94)
+
+(def full-matrix '([94 22 8400] [34 67 5400]))
 
 (def mt1 [94 22 8400])
 (def mt2 [34 67 5400])
 
-(def int1 (-> (matrix-simplify-col mt1 0)
-              (matrix-multiply-by (first mt2))
+(def int1 (-> (row-simplify-col mt1 0)
+              (row-multiply-by (first mt2))
               ))
 
-(-> (matrix-simplify-col mt1 0)
-    (matrix-multiply-by (first mt2))
+(-> (row-simplify-col mt1 0)
+    (row-multiply-by (first mt2))
     )
 
 (defn invert-ratio
   [ratio]
-  (/ (denominator ratio)(numerator ratio)))
+  (/ (denominator ratio) (numerator ratio)))
 
 (invert-ratio 3455/67)
 
 (denominator 23/45)
 
-(def row2 (matrix-subtract mt2 int1))
-(matrix-multiply-by row2 (invert-ratio (second row2)))
-
-
-
-
-
+(def row2 (row-subtract mt2 int1))
+(row-multiply-by row2 (invert-ratio (second row2)))
 
 (raw-line->ints "Button A: X+94, Y+34")
 (raw-line->ints "Button B: X+22, Y+67")
@@ -98,8 +100,8 @@
   [filename]
   (let [raw-strings (io/read-input "day13/example.txt")
         no-blanks (->>
-                   (filter not-blank? raw-strings)
-                   (partition 3))
+                    (filter not-blank? raw-strings)
+                    (partition 3))
         _ (run! println no-blanks)
         mapped-lines (raw-lines->ints (first no-blanks))
         _ (println mapped-lines)
