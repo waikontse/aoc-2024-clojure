aa0758d22745531f4ab17ff11e9d91f9f7e92294
wkt <waikon@waikontse.info>
2025-04-18 15:31:12 +0200

    Add skeleton for day 14

--- src/advent_of_code_2024/week2/day14.clj
+++ src/advent_of_code_2024/week2/day14.clj
@@ -1,1 +1,29 @@
-(ns advent-of-code-2024.week2.day14)
+(ns advent-of-code-2024.week2.day14
+  (:require [advent-of-code-2024.utils.io :as io]
+            [clojure.pprint :as pp])
+  )
+
+(def pOrV #"(p\=|v\=)")
+
+(defn clean-string
+  [line]
+  (clojure.string/replace line pOrV ""))
+
+(defn parse-to-spec
+  [raw-line]
+  (let [clean-line (clean-string raw-line)
+        position "todo"
+        velocity "todo"
+        ]
+    {:position position :velocity velocity})
+  )
+
+(defn solve-part-1
+  "docstring"
+  [filename]
+  (let [lines (io/read-input "day14/example.txt")
+        ;_ (println lines)
+        cleaned-lines (map #(clean-string %) lines)
+        _ (pp/pprint cleaned-lines)
+        ])
+  )
