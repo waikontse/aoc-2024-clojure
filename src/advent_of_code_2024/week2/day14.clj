c797758a73998516d0a1b3dc1652064570e71a6d
wkt <waikon@waikontse.info>
2025-04-19 02:10:07 +0200

    Add day 14 code

--- src/advent_of_code_2024/week2/day14.clj
+++ src/advent_of_code_2024/week2/day14.clj
@@ -1,6 +1,8 @@
 (ns advent-of-code-2024.week2.day14
   (:require [advent-of-code-2024.utils.io :as io]
-            [clojure.pprint :as pp])
+            [clojure.pprint :as pp]
+            [advent-of-code-2024.utils.algorithms :as algo]
+            )
   )
 
 (def pOrV #"(p\=|v\=)")
@@ -9,21 +11,48 @@
   [line]
   (clojure.string/replace line pOrV ""))
 
+(defn splitted-to-pair
+  "E.g. 9,4 -> {:x 9 :y 4}"
+  [raw-splitted]
+  (let [raw-numbers (clojure.string/split raw-splitted #",")
+        left (io/str->int (first raw-numbers))
+        right (io/str->int (second raw-numbers))]
+    [left right]))
+
+
 (defn parse-to-spec
   [raw-line]
   (let [clean-line (clean-string raw-line)
-        position "todo"
-        velocity "todo"
+        splitted (clojure.string/split clean-line #" ")
+        position (splitted-to-pair (first splitted))
+        velocity (splitted-to-pair (second splitted))
         ]
     {:position position :velocity velocity})
   )
 
-(defn solve-part-1
-  "docstring"
-  [filename]
-  (let [lines (io/read-input "day14/example.txt")
-        ;_ (println lines)
-        cleaned-lines (map #(clean-string %) lines)
-        _ (pp/pprint cleaned-lines)
-        ])
+;; multiply the position by velocity
+;; normalize to the grid size
+;; partition to the quadrants
+
+(defn multiply-position
+  ""
+  [robot multiplier]
+  (let [new-values (algo/row-multiply-by multiplier)]
+    )
+
+
   )
+
+(def pos  {:position {:x 9, :y 3}, :velocity {:x 2, :y 3}})
+((comp :x :position) pos)
+
+(defn solve-part-1
+"docstring"
+[filename]
+(let [lines (io/read-input "day14/example.txt")
+                                        ;_ (println lines)
+      cleaned-lines (map #(clean-string %) lines)
+      _ (pp/pprint cleaned-lines)
+      _ (pp/pprint (map #(parse-to-spec %) cleaned-lines))
+      ])
+)
