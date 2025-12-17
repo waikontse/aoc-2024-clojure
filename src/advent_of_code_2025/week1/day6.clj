2c6d22fe373582a387c442c9f7d20de1438d8954
Wai Kon Tse <wai.kon.tse@ing.com>
2025-12-17 20:12:10 +0100

    try to solve day 6 part 2

--- src/advent_of_code_2025/week1/day6.clj
+++ src/advent_of_code_2025/week1/day6.clj
@@ -7,9 +7,13 @@
 
 (defn raw-string-to-ints
   [raw-string]
-  (->> (clojure.string/split (clojure.string/trim raw-string) #" +")
-       (map #(io/str->int %)))
+  (->> (clojure.string/split (clojure.string/trim raw-string) #" {1,1}")
+       ;(map #(io/str->int %))
+       )
   )
+(raw-string-to-ints "123 328  51 64")
+
+(clojure.string/split-lines example)
 
 (raw-string-to-ints "6 7 8 9")
 
@@ -26,13 +30,14 @@
     combined)
   )
 
+(parse-to-raw-lines example)
+
 (println (apply map vector (parse-to-raw-lines example)))
 
 (defn eval-line
-  "docstring"
-  [line]
+  [line transformer]
   (let [op (first line)
-        args (rest line)
+        args (transformer (rest line))
         ]
     (cond
       (= "+" op) (apply + args)
@@ -40,19 +45,51 @@
       ))
   )
 
+;; 4277556
+;; 5335495999141
 (defn solve-part-1
-  "docstring"
   [input]
   (let [parsed-input (apply map vector (parse-to-raw-lines input))
-        ;_ (println parsed-input)
-        eval-parsed-input (map #(eval-line %) parsed-input)
+        eval-parsed-input (map #(eval-line % identity) parsed-input)
         ]
     (apply + eval-parsed-input))
   )
 (solve-part-1 input)
 
 
+(defn transpose-numbers
+  [coll]
+  (let [_ (println "transposing: " coll)
+        str-coll (map #(str %) coll)
+        max-length (apply max (map #(count %) str-coll))
+        transposed (->> (range max-length)
+                        (map (fn [pos]
+                               (map #(get % pos) str-coll)))
+                        (map #(apply str %))
+                        (map #(io/str->int %)))
+        _ (clojure.pprint/pprint transposed)]
+    transposed)
+  )
+
+;; 3263827
+
 (defn solve-part-2
-  "docstring"
   [input]
-  0)
+  (let [parsed-input (apply map vector (parse-to-raw-lines input))
+        eval-parsed-input (map #(eval-line % transpose-numbers) parsed-input)
+        ]
+    (apply + eval-parsed-input))
+  )
+
+;The rightmost problem is 4 + 431 + 623 = 1058
+;The second problem from the right is 175 * 581 * 32 = 3253600
+;The third problem from the right is 8 + 248 + 369 = 625
+;Finally, the leftmost problem is 356 * 24 * 1 = 8544
+;Now, the grand total is 1058 + 3253600 + 625 + 8544 = 3263827.
+
+(solve-part-2 example)
+
+
+
+(transpose-numbers ["64" "23" "314"])
+(transpose-numbers [64 23 314])
