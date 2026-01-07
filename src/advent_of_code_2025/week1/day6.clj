f11b753275d6646f404f6981bd2148da8630848d
Wai Kon Tse <wai.kon.tse@ing.com>
2026-01-07 20:42:21 +0100

    Add solution for day 6 part 2

--- src/advent_of_code_2025/week1/day6.clj
+++ src/advent_of_code_2025/week1/day6.clj
@@ -17,11 +17,9 @@
   [raw-input]
   (let [raw-split-lines (clojure.string/split-lines raw-input)
         raw-split-lines-without-last (drop-last raw-split-lines)
-        ;;_ (println "raw split without last: " raw-split-lines-without-last)
         last-line (clojure.string/split (last raw-split-lines) #" +")
         integer-split-lines (map #(raw-string-to-ints %) raw-split-lines-without-last)
         combined (conj integer-split-lines last-line)
-        ;_ (println  "combined: " combined)
         ]
     combined)
   )
@@ -29,7 +27,6 @@
 (println (apply map vector (parse-to-raw-lines example)))
 
 (defn eval-line
-  "docstring"
   [line]
   (let [op (first line)
         args (rest line)
@@ -45,7 +42,6 @@
 (defn solve-part-1
   [input]
   (let [parsed-input (apply map vector (parse-to-raw-lines input))
-        ;_ (println parsed-input)
         eval-parsed-input (map #(eval-line %) parsed-input)
         ]
     (apply + eval-parsed-input))
@@ -61,57 +57,66 @@
         converted-rest (->> (map #(clojure.string/trim %) rest)
                             (io/strs->ints))
         ]
-    (cons op converted-rest))
-  )
-
-(eval-line (eval-line-2 ["+" "123" " 45" "  6"]))
+    (cons op converted-rest)))
 
 (defn get-nth-for-all
   [nth strings]
   (apply str (map #(get % nth) strings)))
 
-
 (defn prepare-numbers
-  [from to-inclusive lines]
-  (map #(get-nth-for-all % lines) (range from (inc to-inclusive)))
+  [from to-exclusive lines]
+  (map #(get-nth-for-all % lines) (range from to-exclusive))
   )
 
 (defn find-split-indexes
   [lines]
   (let [numbers (drop-last lines)
         ops (clojure.string/split (last lines) #" +")
+        splits (for [x (range 0 (count (first numbers)))
+                     :let [column (get-nth-for-all x numbers)
+                           split x]
+                     :when (clojure.string/blank? column)
+                     ]
+                 split)
         ]
-    (for [x (range 0 (count (first numbers)))
-          :let [column (get-nth-for-all x numbers)
-                split x]
-          :when (clojure.string/blank? column)
-          ]
-      split)
-    )
-  )
+    {:split splits :ops ops}))
+
+(defn insert-start-indexes-between
+  [xs]
+  (reduce (fn [coll val]
+            (-> (conj coll val)
+                (conj (inc val))))
+          [0]
+          xs))
+
+(defn pad-with-end
+  [xs end]
+  (conj xs end))
+
+(defn calculate-number
+  [arg lines]
+  (let [[coord op] arg
+        [low high] coord
+        ]
+    (->> (prepare-numbers low high lines)
+         (cons op)
+         (eval-line-2)
+         (eval-line))))
 
 (defn solve-part-2
-  "docstring"
   [input]
-  0)
+  (let [splitted (clojure.string/split-lines input)
+        splitted-without-ops (drop-last splitted)
+        splits-and-ops (find-split-indexes splitted)
+        max-line-length (apply max (map #(count %) splitted))
+        new-splits-with-end (pad-with-end (insert-start-indexes-between (:split splits-and-ops)) max-line-length)
+        partitions (partition 2 new-splits-with-end)
+        partitions-with-ops (map vector partitions (:ops splits-and-ops))
+        calculated-numbers (map #(calculate-number % splitted-without-ops) partitions-with-ops)
+        sum (apply + calculated-numbers)
+        ]
+    sum)
+  )
 
-
-
-(->> (clojure.string/split-lines example)
-     (drop-last)
-     (prepare-numbers 0 2)
-     (cons "*")
-     (eval-line-2)
-     (eval-line))
-
-
-;; experiments to solve part 2
-
-(find-split-indexes (clojure.string/split-lines example))
-
-(get-nth-for-all 3 (clojure.string/split-lines example))
-(get-nth-for-all 0 ["1 2 3" "4 5 6" "7 8 9"])
-
-(io/str->int (clojure.string/trim "   4"))
-
-(clojure.string/blank? "   ")
+;; 10142723156431
+(solve-part-2 input)
