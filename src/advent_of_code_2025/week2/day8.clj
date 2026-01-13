0576a3f4d7861bdb2a55964e4adc19e5952b0814
wkt <waikon@waikontse.info>
2026-01-13 09:29:32 +0100

    Add start of day 8

--- src/advent_of_code_2025/week2/day8.clj
+++ src/advent_of_code_2025/week2/day8.clj
@@ -10,11 +10,9 @@
   (let [diff-x (- (:x point-from) (:x point-to))
         diff-y (- (:y point-from) (:y point-to))
         diff-z (- (:z point-from) (:z point-to))
-        dist-xy (Math/sqrt (+ (* diff-x diff-x) (* diff-y diff-y)))
-        total-dist (Math/sqrt (+ (* diff-z diff-z) (* dist-xy dist-xy)))
-        ;;_ (println "Distance from:" point-from "to" point-to " ->" total-dist)
+        total-dist (Math/sqrt (+ (* diff-z diff-z) (* diff-x diff-x) (* diff-y diff-y)))
         ]
-    total-dist))
+    {:from point-from :to point-to :distance total-dist}))
 
 (defn calc-distance-all-pairs
   [coll]
@@ -22,28 +20,65 @@
                  (let [rest (drop (inc idx) coll)
                        all-distances (map #(calc-distance item %) rest)
                        ]
-                   {:source item :distances all-distances})
-                 )
+                   all-distances))
                coll)
   )
 
 (defn connect-junction-boxes
   [coll max-connections]
-  (loop [prioritized-dist 0
-         seen #{}
+  (loop [prioritized-dist coll
+         seen {}
          connected-boxes {}
-         connected-count 0
-         ]
-    (let [curr (first prioritized-dist)
-          ]
-      (cond
-        (= max-connections connected-count) connected-boxes
-        (empty? prioritized-dist) (throw (Exception. "Not expected. Limit not reached, but targets are finished."))
-        (or (contains? seen (:from curr)) (contains? seen (:to curr))) (recur (rest prioritized-dist) seen connected-boxes connected-count)
-        :else (throw (Exception. "Implement me"))        )
+         connected-count 0]
+    (cond
+      (= max-connections connected-count) connected-boxes
+      (empty? prioritized-dist) (throw (Exception. "Not expected. Limit not reached, but targets are finished."))
+
+      :else
+      (let [curr (first prioritized-dist)
+            from (:from curr)
+            to (:to curr)
+            from-is-seen? (contains? seen from)
+            to-is-seen? (contains? seen to)
+            from-connected-to (get seen from)
+            to-connected-to (get seen to)
+            _ (clojure.pprint/pprint connected-boxes)
+            _ (clojure.pprint/pprint seen)
+            _ (println "connected-count:" connected-count)
+            ]
+        (cond
+          ;; :from is connected, and :to is connected -> continue without doing anything
+          (and from-is-seen? to-is-seen?) (recur
+                                            (rest prioritized-dist)
+                                            seen
+                                            connected-boxes
+                                            connected-count)
+          ;; Connect when :from is connected, and :to is unconnected -> connect to from
+          (and from-is-seen? (false? to-is-seen?)) (recur
+                                                     (rest prioritized-dist)
+                                                     (assoc seen to from-connected-to)
+                                                     (assoc connected-boxes from-connected-to (conj (get connected-boxes from-connected-to) to))
+                                                     (inc connected-count))
+          ;; Connect when :from is unconnected, and to is connected
+          (and (false? from-is-seen?) to-is-seen?) (recur
+                                                     (rest prioritized-dist)
+                                                     (assoc seen from to-connected-to)
+                                                     (assoc connected-boxes to-connected-to (conj (get connected-boxes to-connected-to) from))
+                                                     (inc connected-boxes))
+
+          ;; Connect when :from is unconnected, and :to is unconnected
+          (and (false? from-is-seen?) (false? to-is-seen?)) (recur
+                                                              (rest prioritized-dist)
+                                                              (assoc seen from from to from)
+                                                              (assoc connected-boxes (:from curr) (conj (get connected-boxes (:from curr)) (:to curr)))
+                                                              (+ connected-count 2))
+          )
+        )
       )
     )
   )
+
+
 
 (calc-distance
   {:x 0 :y 0 :z 0}
@@ -68,9 +103,14 @@
         points (parse-lines-to-points raw-lines)
         _ (clojure.pprint/pprint points)
         all-distances (calc-distance-all-pairs points)
-        _ (clojure.pprint/pprint all-distances)
+        all-distances-combined (flatten all-distances)
+        ;_ (clojure.pprint/pprint all-distances-combined)
+        sorted-all-distances-combined (sort-by #(:distance %) all-distances-combined)
+        _ (clojure.pprint/pprint sorted-all-distances-combined)
+        connected (connect-junction-boxes sorted-all-distances-combined 10)
+        _ (clojure.pprint/pprint connected)
         ]
-    )
+    0)
   )
 
 (solve-part-1 example)
