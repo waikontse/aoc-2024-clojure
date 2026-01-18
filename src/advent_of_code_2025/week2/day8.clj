92ba0bda5da84939595f3e10673e6044e4d12580
wkt <waikon@waikontse.info>
2026-01-18 02:20:51 +0100

    Add solution day 8

--- src/advent_of_code_2025/week2/day8.clj
+++ src/advent_of_code_2025/week2/day8.clj
@@ -1,7 +1,6 @@
 (ns advent-of-code-2025.week2.day8
   (:require [advent-of-code-2024.utils.io :as io]))
 
-
 (def example (slurp "./resources/y2025/day8/example.txt"))
 (def input (slurp "./resources/y2025/day8/input.txt"))
 
@@ -24,15 +23,38 @@
                coll)
   )
 
+(defn merge-circuits
+  "Merge all the nodes connect to the to node, to the from node."
+  [from to circuits seen]
+  (let [_ (println "connecting from" from " to " to)
+        from-connected-to (get seen from)
+        to-connected-to (get seen to)
+        _ (println "from-connected-to" "to-connected-to" from-connected-to to-connected-to)
+        from-circuit (get circuits from-connected-to)
+        to-circuit (get circuits to-connected-to)
+        combined-circuit (conj (into from-circuit to-circuit) to-connected-to)
+        updated-circuits (-> (assoc circuits from-connected-to combined-circuit)
+                             (dissoc to-connected-to))
+        updated-seen (-> (reduce (fn [coll item] (assoc coll item from-connected-to))
+                                 seen
+                                 to-circuit)
+                         (assoc to from-connected-to to-connected-to from-connected-to)
+                         )
+        ]
+    {:updated-circuits updated-circuits :updated-seen updated-seen})
+  )
+
 (defn connect-junction-boxes
-  [coll max-connections]
-  (loop [prioritized-dist coll
+  [coll max-pairs nodes]
+  (loop [prioritized-dist (take max-pairs coll)
          seen {}
-         connected-boxes {}
-         connected-count 0]
+         circuits {}
+         last-connection {}]
     (cond
-      (<= max-connections connected-count) (do (println "Finished, connected:" connected-count) connected-boxes)
-      (empty? prioritized-dist) connected-boxes
+      (empty? prioritized-dist) (do (println "Finishes connecting all pairs of" max-pairs "seen" (count seen))
+                                    ;  (println "Seen: " seen)
+                                    circuits)
+       (and (= (count seen) nodes) (= 1 (count circuits))) (do (println "finished connecting all." last-connection) last-connection)
 
       :else
       (let [curr (first prioritized-dist)
@@ -42,47 +64,54 @@
             to-is-seen? (contains? seen to)
             from-connected-to (get seen from)
             to-connected-to (get seen to)
-            _ (clojure.pprint/pprint connected-boxes)
+            ;_ (clojure.pprint/pprint circuits)
             ;_ (clojure.pprint/pprint seen)
-            _ (println "connected-count:" connected-count)
+            _ (println "seen:" (count seen))
             ]
         (cond
-          ;; :from is connected, and :to is connected -> continue without doing anything
-          (and from-is-seen? to-is-seen?) (recur
-                                            (rest prioritized-dist)
-                                            seen
-                                            connected-boxes
-                                            connected-count)
+          ;; :from and to are both connected to the same circuits -> continue without doing anything
+          (and from-is-seen? to-is-seen? (= from-connected-to to-connected-to)) (do
+                                                                                  (println "**skipping because both seen same**" from to)
+                                                                                  (recur
+                                                                                    (rest prioritized-dist)
+                                                                                    seen
+                                                                                    circuits
+                                                                                    {}))
+          ;; :from and to is connected on different circuits -> merge the 2 different circuits
+          (and from-is-seen? to-is-seen? (not= from-connected-to to-connected-to)) (do
+                                                                                     (println "**Merging 2 connected boxes**" from to)
+                                                                                     (let [updated-vals (merge-circuits from to circuits seen)]
+                                                                                       (recur
+                                                                                         (rest prioritized-dist)
+                                                                                         (:updated-seen updated-vals)
+                                                                                         (:updated-circuits updated-vals)
+                                                                                         {:from from :to to}
+                                                                                         )))
           ;; Connect when :from is connected, and :to is unconnected -> connect to from
           (and from-is-seen? (false? to-is-seen?)) (recur
                                                      (rest prioritized-dist)
                                                      (assoc seen to from-connected-to)
-                                                     (assoc connected-boxes from-connected-to (conj (get connected-boxes from-connected-to) to))
-                                                     (inc connected-count))
+                                                     (assoc circuits from-connected-to (conj (get circuits from-connected-to) to))
+                                                     {:from from :to to})
           ;; Connect when :from is unconnected, and to is connected
           (and (false? from-is-seen?) to-is-seen?) (recur
                                                      (rest prioritized-dist)
                                                      (assoc seen from to-connected-to)
-                                                     (assoc connected-boxes to-connected-to (conj (get connected-boxes to-connected-to) from))
-                                                     (inc connected-count))
+                                                     (assoc circuits to-connected-to (conj (get circuits to-connected-to) from))
+                                                     {:from from :to to})
 
           ;; Connect when :from is unconnected, and :to is unconnected
           (and (false? from-is-seen?) (false? to-is-seen?)) (recur
                                                               (rest prioritized-dist)
                                                               (assoc seen from from to from)
-                                                              (assoc connected-boxes (:from curr) (conj (get connected-boxes (:from curr)) (:to curr)))
-                                                              (+ connected-count 2))
+                                                              (assoc circuits from (conj (get circuits from) to))
+                                                              {:from from :to to})
           )
         )
       )
     )
   )
 
-
-
-(calc-distance
-  {:x 0 :y 0 :z 0}
-  {:x 1 :y 1 :z 1})
 
 (defn parse-line-to-point
   [raw-line]
@@ -95,10 +124,8 @@
   [raw-lines]
   (map #(parse-line-to-point %) raw-lines))
 
-
-
 (defn solve-part-1
-  [input limit]
+  [input max-pairs nodes]
   (let [raw-lines (clojure.string/split-lines input)
         points (parse-lines-to-points raw-lines)
         ;_ (clojure.pprint/pprint points)
@@ -106,18 +133,19 @@
         all-distances-combined (flatten all-distances)
         ;_ (clojure.pprint/pprint all-distances-combined)
         sorted-all-distances-combined (sort-by #(:distance %) all-distances-combined)
-        ;_ (clojure.pprint/pprint sorted-all-distances-combined)
-        connected (connect-junction-boxes sorted-all-distances-combined limit)
-        _ (clojure.pprint/pprint connected)
+        ; _ (clojure.pprint/pprint (take 15 sorted-all-distances-combined))
+        connected (connect-junction-boxes sorted-all-distances-combined max-pairs nodes)
+        ;_ (clojure.pprint/pprint connected)
         result (->> (vals connected)
                     (map #(inc (count %)))
                     (sort)
                     (reverse)
-                    (take 3)
-                    (apply *))
+                    (take 3))
         _ (println "result" result)
         ]
-    result)
+    (apply * result))
   )
 
-(solve-part-1 example 10)
+
+(solve-part-1 input 1000000 1000)
+;; 6095621910
