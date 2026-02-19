4dcade73b051c2fc4497ffe783ef6601eb10c612
Wai Kon Tse <wai.kon.tse@ing.com>
2026-02-19 01:27:25 +0100

    add skeleton code for day 10

---
+++ src/advent_of_code_2025/week2/day10.clj
@@ -0,0 +1,17 @@
+(ns advent-of-code-2025.week2.day10)
+
+(def example (slurp "./resources/y2025/day10/example.txt"))
+(def input (slurp "./resources/y2025/day10/input.txt"))
+
+
+(defn parse-raw-line-to-puzzle
+  "docstring"
+  [arglist]
+  )
+
+(defn parse-raw-lines-to-puzzle
+  "docstring"
+  [arglist]
+  )
+
+example
