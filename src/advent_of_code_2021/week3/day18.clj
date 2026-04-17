1984c552944aab63b028a942c87fd7ef0a4a6a7f
wkt <waikon@waikontse.info>
2026-04-17 22:49:57 +0200

    Add code for day 18 year 2021

--- src/advent_of_code_2021/week3/day18.clj
+++ src/advent_of_code_2021/week3/day18.clj
@@ -1,55 +1,139 @@
-(ns advent-of-code-2021.week3.day18)
+(ns advent-of-code-2021.week3.day18
+  (:require [clojure.math.combinatorics :as combo]
+            )
+  )
 
 (def example (slurp "./resources/y2021/week3/day18/example.txt"))
+(def example2 (slurp "./resources/y2021/week3/day18/example2.txt"))
 (def input (slurp "./resources/y2021/week3/day18/input.txt"))
 (def simple-pair-regex #"\[(\d+),(\d+)]")
 (def simple-pair-pattern (re-pattern "\\[\\d+,\\d+]"))
 (def digits-matcher (re-pattern "\\d+"))
 
-(re-find simple-pair-pattern "[[[[[9,8],1],2],3],4]")
-(re-find simple-pair-pattern "[]")
-
-(println example)
-
-
 (defn add-number
-  "docstring"
   [left right]
   (str "[" left "," right "]"))
 
-(defn reduce-number
-  "docstring"
+(defn can-explode-number?
   [number]
-  ;; Explode number until no more explodes are possible, then split number until no more splits are possible, then repeat until no more explodes or splits are possible.
+  (let [length (count number)]
+    (loop [currPos 0
+           currLevel 0]
+      (cond
+        (>= currLevel 5) true
+        (= currPos length) false
+        (= \[ (get number currPos)) (recur (inc currPos) (inc currLevel))
+        (= \] (get number currPos)) (recur (inc currPos) (dec currLevel))
+        (< currLevel 0) (throw (RuntimeException. "currLevel is negative"))
+        :else (recur (inc currPos) currLevel)))))
+
+(defn find-explotable-number
+  "Returns the start and ending position with match E.g. {:match <string> :start <int> :end <int> }"
+  [number]
+  (let [length (count number)]
+    (loop [currPos 0
+           currLevel 0
+           currStart 0
+           currEnd 0
+           matched nil]
+      (cond
+        ;; Stop conditions
+        (< currLevel 0) (throw (RuntimeException. "currLevel is negative"))
+        (>= currPos length) (throw (RuntimeException. (clojure.string/join ", " ["could not find explotable number" currPos currLevel currStart currEnd matched])))
+        (and (= currLevel 4) (> currEnd 0)) {:match matched :start currStart :end currEnd}
+
+        ;; intermediate conditions
+        (and (< currLevel 5) (= \[ (get number currPos))) (recur (inc currPos) (inc currLevel) 0 0 nil)
+        (and (< currLevel 5) (= \] (get number currPos))) (recur (inc currPos) (dec currLevel) 0 0 nil)
+        (and (< currLevel 5) (= 0 currEnd)) (recur (inc currPos) currLevel 0 0 nil)
+
+        ;; Start the matching and setting the start / end values
+        (and (= currLevel 5) (= \[ (get number (dec currPos)))) (recur (inc currPos) 5 (dec currPos) 0 (str \[ (get number currPos)))
+        (and (= currLevel 5) (not= \] (get number currPos))) (recur (inc currPos) 5 currStart 0 (str matched (get number currPos)))
+        (and (= currLevel 5) (= \] (get number currPos))) (recur (inc currPos) 4 currStart currPos (str matched \]))
+
+        :else (throw (RuntimeException. (clojure.string/join ", " ["unexpected error while finding explotable number" currPos currLevel currStart currEnd matched]))))))
   )
 
-(defn explode-number
-  "docstring"
-  [number]
-  )
 
 (defn find-all-numbers
   [number]
   (let [matcher (re-matcher digits-matcher number)]
     (loop [results []]
       (if (.find matcher)
-        (recur (conj results {:match (.group matcher)
+        (recur (conj results {:match (Integer/parseInt (str (.group matcher)))
                               :start (.start matcher)
-                              :end (.end matcher)}))
+                              :end   (.end matcher)}))
         results))))
 
 (defn can-split-number?
   [number]
   (let [digit-matches (find-all-numbers number)]
     (->> (map #(:match %) digit-matches)
-         (some #(>= (Integer/parseInt (str %)) 10)))))
+         (some #(>= % 10)))))
 
-(can-split-number? "[[[[2,7],4],[9,[10,1]]],[1,1]]")
+(defn replace-string-index
+  [s replace start-index stop-index]
+  (let [left-side (subs s 0 start-index)
+        right-side (subs s stop-index)
+        new-string (str left-side replace right-side)
+        ]
+    new-string))
 
 (defn split-number
   "docstring"
-  [arglist]
-  )
+  [number-to-split]
+  (let [leftmost-number-to-split (->> (find-all-numbers number-to-split)
+                                      (filter #(>= (:match %) 10))
+                                      first)
+        leftmost-number (:match leftmost-number-to-split)
+        splitted-number (str "[" (quot leftmost-number 2) "," (^[int int] Math/ceilDiv leftmost-number 2) "]")
+        start-index (:start leftmost-number-to-split)
+        end-index (:end leftmost-number-to-split)
+        new-number (replace-string-index number-to-split splitted-number start-index end-index)
+        ]
+    new-number))
+
+(defn explode-number
+  "docstring"
+  [number]
+  (let [explotable-number (find-explotable-number number)
+        [_, explotable-number-left, _] (re-matches simple-pair-regex (:match explotable-number))
+        [_, _, explotable-number-right] (re-matches simple-pair-regex (:match explotable-number))
+        number-left (last (find-all-numbers (subs number 0 (:start explotable-number))))
+        number-right (first (find-all-numbers (subs number (:end explotable-number))))
+        new-number-left (if (nil? number-left) nil (+ (:match number-left) (Integer/parseInt explotable-number-left)))
+        new-number-right (if (nil? number-right) nil (+ (:match number-right) (Integer/parseInt explotable-number-right)))
+        ;; Replace number on the right
+        number-replaced-on-right (if (nil? new-number-right)
+                                   number
+                                   (replace-string-index number new-number-right
+                                                         (+ (:end explotable-number) (:start number-right))
+                                                         (+ (:end explotable-number) (:end number-right))))
+        ;; Replace number on the left
+        number-replaced-on-left (if (nil? new-number-left)
+                                  number-replaced-on-right
+                                  (replace-string-index number-replaced-on-right new-number-left
+                                                        (:start number-left)
+                                                        (:end number-left)))
+
+        ;; Find the explotable pair again and replace with 0
+        explotable-number (find-explotable-number number-replaced-on-left)
+        pair-replaced-with-0 (replace-string-index number-replaced-on-left "0" (:start explotable-number) (inc (:end explotable-number)))
+        ]
+    pair-replaced-with-0))
+
+(defn reduce-number
+  "docstring"
+  [number]
+  ;; Explode number until no more explodes are possible, then split number until no more splits are possible,
+  ;; then repeat until no more explodes or splits are possible.
+  (loop [number-to-reduce number]
+    (cond
+      (true? (can-explode-number? number-to-reduce)) (recur (explode-number number-to-reduce))
+      (true? (can-split-number? number-to-reduce)) (recur (split-number number-to-reduce))
+      :else number-to-reduce
+      )))
 
 (defn find-simple-pair
   "Find the location of the first pair [\\d+,\\d+]. Return the pair and its location in the number."
@@ -58,14 +142,11 @@
     first-simple-pair))
 
 (defn magnitude-calc
-  "docstring"
   [simple-pair]
   (cond
     (nil? simple-pair) nil
     :else (let [[_, left, right] (re-matches simple-pair-regex simple-pair)]
             (+ (* 3 (Integer/parseInt left)) (* 2 (Integer/parseInt right))))))
-
-(magnitude-calc "[9,1]")
 
 (defn magnitude
   "docstring"
@@ -78,38 +159,32 @@
         :else (recur (clojure.string/replace-first current-number first-simple-pair pair-value))
         ))))
 
-(magnitude "[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]")
-;
-;Here are a few more magnitude examples:
-;
-;[[1,2],[[3,4],5]] becomes 143.
-;[[[[0,7],4],[[7,8],[6,0]]],[8,1]] becomes 1384.
-;[[[[1,1],[2,2]],[3,3]],[4,4]] becomes 445.
-;[[[[3,0],[5,3]],[4,4]],[5,5]] becomes 791.
-;[[[[5,0],[7,4]],[5,5]],[6,6]] becomes 1137.
-;[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]] becomes 3488.
+(defn add-and-reduce-number
+  [left right]
+  (let [reduced-left (reduce-number left)
+        reduced-right (reduce-number right)
+        added-number (add-number reduced-left reduced-right)]
+    added-number))
 
+(defn add-and-reduce-all-numbers
+  [numbers]
+  (-> (reduce (fn [coll item]
+                (add-and-reduce-number coll item))
+              (first numbers)
+              (rest numbers))
+      (reduce-number)))
 
+(defn solve-part-1
+  [splitted-lines]
+  (let [reduced-input (add-and-reduce-all-numbers splitted-lines)
+        magnitude (magnitude reduced-input)]
+    magnitude))
 
-;For example, the final sum of this list is [[[[1,1],[2,2]],[3,3]],[4,4]]:
-;
-;[1,1]
-
-;[2,2]
-;[3,3]
-;[4,4]
-;The final sum of this list is [[[[3,0],[5,3]],[4,4]],[5,5]]:
-;
-;[1,1]
-;[2,2]
-;[3,3]
-;[4,4]
-;[5,5]
-;The final sum of this list is [[[[5,0],[7,4]],[5,5]],[6,6]]:
-;
-;[1,1]
-;[2,2]
-;[3,3]
-;[4,4]
-;[5,5]
-;[6,6]
+(defn solve-part-2
+  [input]
+  (let [splitted-lines (set (clojure.string/split-lines input))
+        all-combos (combo/combinations splitted-lines 2)
+        max-value (->> (pmap #(solve-part-1 %) all-combos)
+                       (apply max))
+        ]
+    max-value))
--- test/advent_of_code_2021/week3/day18_test.clj
+++ test/advent_of_code_2021/week3/day18_test.clj
@@ -9,26 +9,68 @@
 
 (deftest magnitude-test
   (testing "magnitude"
-    (is (= 143 (magnitude "[[1,2],[[3,4],5]]"))))
-  (testing "magnitude"
-    (is (= 1384 (magnitude "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"))))
-  (testing "magnitude"
-    (is (= 445 (magnitude "[[[[1,1],[2,2]],[3,3]],[4,4]]"))))
-  (testing "magnitude"
-    (is (= 791 (magnitude "[[[[3,0],[5,3]],[4,4]],[5,5]]"))))
-  (testing "magnitude"
-    (is (= 1137 (magnitude "[[[[5,0],[7,4]],[5,5]],[6,6]]"))))
-  (testing "magnitude"
-    (is (= 3488 (magnitude "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"))))
+    (are [input expected] (= expected (magnitude input))
+      "[[1,2],[[3,4],5]]" 143
+      "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]" 1384
+      "[[[[1,1],[2,2]],[3,3]],[4,4]]" 445
+      "[[[[3,0],[5,3]],[4,4]],[5,5]]" 791
+      "[[[[5,0],[7,4]],[5,5]],[6,6]]" 1137
+      "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]" 3488)))
+
+(deftest split-number-test
+  (testing "split examples"
+    (are [input expected] (= expected (split-number input))
+      "[[[[0,7],4],[15,[0,13]]],[1,1]]" "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+      "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]" "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]")))
+
+(deftest reduce-number-test
+  (testing "continuously reduce-number"
+    (is (= "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]" (reduce-number "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"))))
+  )
+
+(deftest explode-number-test
+  (testing "explode examples"
+    (are [input expected]
+      (= expected (explode-number input))
+      "[[[[[9,8],1],2],3],4]" "[[[[0,9],2],3],4]"
+      "[7,[6,[5,[4,[3,2]]]]]" "[7,[6,[5,[7,0]]]]"
+      "[[6,[5,[4,[3,2]]]],1]" "[[6,[5,[7,0]]],3]"
+      "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+      "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]" "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+      )))
+
+(deftest add-number-and-reduce
+  (testing
+    (is (= "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]" (-> (add-number "[[[[4,3],4],4],[7,[[8,4],9]]]" "[1,1]")
+                                                   reduce-number))))
+  )
+
+(deftest can-explode-number?-test
+  (testing
+    (is (= true (can-explode-number? "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"))))
+  (testing
+    (is (= false (can-explode-number? "[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]]"))))
+  )
+
+(deftest find-explotable-number-test
+  (testing
+    (is (= {:match "[2,7]", :start 4, :end 8} (find-explotable-number "[[[[[2,7],4],[9,[10,1]]],[1,1]]]"))))
+  (testing
+    (is (= {:match "[3,2]", :start 10, :end 14} (find-explotable-number "[[6,[5,[4,[3,2]]]],1]"))))
   )
 
 
+(deftest solve-part-1-test
+  (testing
+    (is (= 4289 (solve-part-1 (clojure.string/split-lines input)))))
+  (testing
+    (is (= 3488 (solve-part-1 (clojure.string/split-lines example)))))
+  )
 
-
-;after addition: [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]
-;after explode:  [[[[0,7],4],[7,[[8,4],9]]],[1,1]]
-;after explode:  [[[[0,7],4],[15,[0,13]]],[1,1]]
-;after split:    [[[[0,7],4],[[7,8],[0,13]]],[1,1]]
-;after split:    [[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]
-;after explode:  [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
+(deftest solve-part-2-test
+  (testing
+    (is (= 3993 (solve-part-2 example2))))
+  (testing
+    (is (= 4807 (solve-part-2 input))))
+  )
 
